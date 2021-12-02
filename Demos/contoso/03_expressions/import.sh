44fe8fb44d4a06749a54e1a3e2a34d54a9575d2a
Suren Mohandass <sumohand@microsoft.com>
2021-12-02 15:30:37 +0000

    Added import.sh

---
+++ Demos/contoso/03_expressions/import.sh
@@ -0,0 +1,3 @@
+terraform import azurerm_resource_group.rgs[0] "/subscriptions/.../resourceGroups/contoso_research_dev_rg"
+terraform import azurerm_resource_group.rgs[1] "/subscriptions/.../resourceGroups/contoso_research_staging_rg"
+terraform import azurerm_resource_group.rgs[2] "/subscriptions/.../resourceGroups/contoso_research_prod_rg"
