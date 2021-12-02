30dbe71712e1f17ebbcf56f825459ecff0d13b00
Suren Mohandass <sumohand@microsoft.com>
2021-12-02 15:30:16 +0000

    Added main.tf

---
+++ Demos/contoso/02_config/main.tf
@@ -0,0 +1,64 @@
+# locals {
+#     prefix = "contoso"
+#     region = "UK South"
+#     tags = {
+#         cost_center = "contoso research"
+#     }
+# }
+
+# provider "azurerm" {
+#     version = "~>2.13.0"
+#     features {}    
+# }
+
+# resource "azurerm_resource_group" "contoso_rg" {
+#     name = "${local.prefix}_rg"
+#     location = local.region
+#     tags = local.tags
+# }
+
+
+# resource "azurerm_resource_group" "contoso_dev_rg" {    
+#     name = "${local.prefix}_dev_rg"
+#     location = local.region
+#     tags = local.tags
+# }
+
+
+# -------
+
+# variable prefix {}
+
+# variable region {           
+#     type = string
+#     default = "North Europe"
+# }
+
+# variable tags {
+#     type= map          
+# }
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "contoso_rg" {
+  name     = "${var.prefix}_rg"
+  location = var.region
+  tags     = var.tags
+}
+
+resource "azurerm_resource_group" "contoso_dev_rg" {
+  name     = "${var.prefix}_dev_rg"
+  location = var.region
+  tags     = var.tags
+}
