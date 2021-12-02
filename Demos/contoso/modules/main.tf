3e378727e00594f9dc9de2fff7edcfb34a45f1ed
Suren Mohandass <sumohand@microsoft.com>
2021-12-02 15:33:04 +0000

    Updated 4 files under /Demos/contoso/modules

--- Demos/contoso/modules/main.tf
+++ Demos/contoso/modules/main.tf
@@ -1,1 +1,31 @@
-# test
+# main.tf from calling module 
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+
+module "connectedrg" {
+  
+  source = "./modules/contoso-az-connectedrg" 
+
+  # or remote git repo with ?ref=verion    
+  # notice the //
+  #source   = "<repo-server>/<username>/<repo-name>//contoso-az-connectedrg?ref=v1.0"
+  
+  rg_names = var.rg_names
+  vnets    = var.vnets
+}
+
+
+
+
+
