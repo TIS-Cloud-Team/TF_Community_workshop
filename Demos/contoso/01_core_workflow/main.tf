cffeac6ec2566d03b0517e0b4b05a88ac99686ef
Suren Mohandass <sumohand@microsoft.com>
2021-12-02 15:29:48 +0000

    Added main.tf

---
+++ Demos/contoso/01_core_workflow/main.tf
@@ -0,0 +1,38 @@
+# core_workflow
+
+# Specifiy the provider and version
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "contoso_rg" {
+  name     = "contoso_rg"
+  location = "UK South"
+
+  tags = {
+    "cost_center" = "contoso research"
+  }
+}
+
+resource "azurerm_resource_group" "contoso_rg_dev" {
+  name     = "contoso_rg_dev"
+  location = "UK South"
+
+  tags = {
+    "cost_center" = "contoso research again"
+  }
+}
+
+
+# az group list | jq '.[] | select(.name == "contoso_rg")'
+# az group list | jq '.[].name'
