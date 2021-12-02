bd2c8e1c8ad775e6f379062e3f1a6510e3dcb9a4
Suren Mohandass <sumohand@microsoft.com>
2021-12-02 15:34:52 +0000

    Updated 2 files under /Demos/contoso/08_remote_state

--- Demos/contoso/08_remote_state/main.tf
+++ Demos/contoso/08_remote_state/main.tf
@@ -1,1 +1,28 @@
-# test
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "contoso"
+    storage_account_name = "tfdemos"
+    container_name       = "livedemo"
+    key                  = "terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "contoso_rg" {
+  name     = "contoso_live_demo_rg"
+  location = "UK South"
+
+  tags = {
+    "purpose" = "remote state"
+  }
+}
